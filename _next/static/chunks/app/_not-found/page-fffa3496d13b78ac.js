8d8d0b6c6299a5e43fcbfc384ba1711b34b72c4b
Kyle Buzbee <kyle.thomas.buzbee@gmail.com>
2025-10-17 00:16:16 -0500

    Deploy K&L Recycling website with Next.js static export

---
+++ _next/static/chunks/app/_not-found/page-fffa3496d13b78ac.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[492],{6347:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_not-found/page",function(){return u(9520)}])}},_=>{_.O(0,[96,441,358],()=>_(_.s=6347)),_N_E=_.O()}]);
